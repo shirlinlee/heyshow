4117d5a5a7cec40e57785f46b1f4633946f108f8
shirlinlee <hn85412404@gmail.com>
2022-01-23 12:39:28 +0800

    [Update] 選單

--- js/index.js
+++ js/index.js
@@ -9,16 +9,36 @@ const index = {
     });
   },
   navHandler: function () {
-    // $(this).toogleClass("active");
+    $("body").on("click", ".menu-open-button", function () {
+      $(".menu-open-button,.nav").toggleClass("active");
+    });
+
+    $("body").on("click", ".sub_nav_btn", function () {
+      $(".sub_nav_btn,.sub_nav").toggleClass("active");
+    });
+
+    $("body").on("click", ".redirect", function () {
+      $(".menu-open-button,.nav,.sub_nav_btn,.sub_nav").removeClass("active");
+    });
+  },
+  anchorHandler: function () {
+    var $body = window.opera
+      ? document.compatMode == "CSS1Compat"
+        ? $("html")
+        : $("body")
+      : $("html,body");
+    $("#tab").on("click", "li", function () {
+      var anchor = $(this).attr("data-anchor");
+      if (anchor) {
+        console.log(anchor);
+        $body.animate({ scrollTop: $(`#${anchor}`).offset().top - 70 }, 700);
+      }
+    });
   },
 };
 
 $(function () {
   index.slickInit();
-
-  $("body").on("click", ".menu-open-button", function () {
-    console.log(123);
-
-    $(this).toggleClass("active");
-  });
+  index.navHandler();
+  index.anchorHandler();
 });
